cd922a47a32d6e8741d69ffe3beccc06e2b47b04
Jordan Guymon <jogu.sd@gmail.com>
2023-08-28 02:42:15 +0900

    Add vial configs

---
+++ keyboards/wilba_tech/rama_works_m60_a/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xD0, 0x3B, 0x7B, 0x16, 0x2A, 0xC4, 0xBC, 0xC7}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 12 }
